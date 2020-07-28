74d90b66dc84f3d564c065a2f2ed263644fb7ba4
Chrish Dunne <hey@chrish.design>
2020-07-28 19:13:10 +0100

    Having another attempt

--- index.js
+++ index.js
@@ -1,213 +1,87 @@
-import gsap from "gsap";
+let velX = 3;
+let velY = 2;
+const container = document.getElementById("alumni");
 
 // ====
-// This controls the linking lines
 
-window.addEventListener("load", function () {
-  const pathA = document.querySelector(".pathA");
-  const textA = document.querySelector(".textA");
-  const pathB = document.querySelector(".pathB");
-  const textB = document.querySelector(".textB");
-  const pathC = document.querySelector(".pathC");
+function setRandomPosition() {
+  const cntWidth = container.offsetWidth / 2;
+  const cntWidthNeg = (container.offsetWidth / 2) * -1;
 
-  const aToB = () => {
-    const coordsA = boxA.getBoundingClientRect();
-    const coordsB = boxB.getBoundingClientRect();
+  const cntHeight = container.offsetHeight / 2;
+  const cntHeightNeg = (container.offsetHeight / 2) * -1;
 
-    const axCenter = coordsA.x + coordsA.width / 2;
-    const ayCenter = coordsA.y + coordsA.height / 2;
-    const bxCenter = coordsB.x + coordsB.width / 2;
-    const byCenter = coordsB.y + coordsB.width / 2;
-    pathA.setAttribute(
-      "d",
-      `
-      M${bxCenter} ${byCenter}
-      L${axCenter} ${ayCenter}
-    `
-    );
+  const randWidth =
+    Math.floor(Math.random() * (cntWidth - cntWidthNeg)) + cntWidthNeg;
+  const randHeight =
+    Math.floor(Math.random() * (cntHeight - cntHeightNeg)) + cntHeightNeg;
 
-    textA.setAttribute("x", `${coordsA.x / 2}px`);
+  const opts = {
+    cntWidth: cntWidth,
+    cntWidthNeg: cntWidthNeg,
+    cntHeight: cntHeight,
+    cntHeightNeg: cntHeightNeg,
+    randWidth: randWidth,
+    randHeight: randHeight,
   };
 
-  const bToC = () => {
-    const coordsA = boxB.getBoundingClientRect();
-    const coordsB = boxC.getBoundingClientRect();
+  return opts;
+}
 
-    const axCenter = coordsA.x + coordsA.width / 2;
-    const ayCenter = coordsA.y + coordsA.height / 2;
-    const bxCenter = coordsB.x + coordsB.width / 2;
-    const byCenter = coordsB.y + coordsB.width / 2;
-    pathB.setAttribute(
-      "d",
-      `
-      M${bxCenter} ${byCenter}
-      L${axCenter} ${ayCenter}
-    `
+// ====
+
+function hitBox(el, bnd) {
+  if (el.offsetLeft <= 0 && velX < 0) {
+    velX = -1 * velX;
+  }
+
+  if (el.offsetLeft + el.offsetWidth >= bnd.offsetWidth) {
+    velX = -1 * velX;
+  }
+
+  if (el.offsetTop <= 0 && velY < 0) {
+    velY = -1 * velY;
+  }
+
+  if (el.offsetTop + el.offsetHeight >= bnd.offsetHeight) {
+    velY = -1 * velY;
+  }
+}
+
+// ====
+
+function moveIt(el, bnd) {
+  hitBox(el, bnd);
+  el.style.left = `${el.offsetLeft + velX}px`;
+  el.style.top = `${el.offsetTop + velY}px`;
+
+  setTimeout(() => {
+    moveIt(el, bnd);
+  }, 25);
+}
+
+// ====
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 1. Find all of our images
+  const images = document.getElementsByClassName("box");
+  let image = null;
+
+  // 2. Do some stuff to our images
+  for (image of images) {
+    // 1. Set a random position
+    image.setAttribute(
+      "style",
+      `left: ${setRandomPosition("alumni").randWidth}px`
+    );
+    image.setAttribute(
+      "style",
+      `top: ${setRandomPosition("alumni").randHeight}px`
     );
 
-    textB.setAttribute("x", `${coordsA.x * 1.1}px`);
-  };
-
-  const CtoA = () => {
-    const coordsA = boxC.getBoundingClientRect();
-    const coordsB = boxA.getBoundingClientRect();
-
-    const axCenter = coordsA.x + coordsA.width / 2;
-    const ayCenter = coordsA.y + coordsA.height / 2;
-    const bxCenter = coordsB.x + coordsB.width / 2;
-    const byCenter = coordsB.y + coordsB.width / 2;
-    pathC.setAttribute(
-      "d",
-      `
-      M${bxCenter} ${byCenter}
-      L${axCenter} ${ayCenter}
-    `
-    );
-  };
-
-  // ====
-  // This is for all of the box animations
-
-  const boxes = document.getElementsByClassName("box");
-  const boxA = boxes.item(0);
-  const boxB = boxes.item(1);
-  const boxC = boxes.item(2);
-
-  const tlA = gsap.timeline({
-    onUpdate: aToB,
-    repeat: -1,
-  });
-
-  tlA
-    // Snap to an initial position
-    // loop through positions until we get
-    // back to the inital position again
-    .to(boxA, {
-      ease: "none",
-      duration: 0,
-      x: "10%",
-      y: "10%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 4,
-      x: "30%",
-      y: "0%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 4,
-      x: "50%",
-      y: "10%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 4,
-      x: "70%",
-      y: "20%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 4,
-      x: "90%",
-      y: "30%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 2,
-      x: "100%",
-      y: "35%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 12,
-      x: "50%",
-      y: "100%",
-    })
-    .to(boxA, {
-      ease: "none",
-      duration: 12,
-      x: "10%",
-      y: "10%",
-    });
-
-  const tlB = gsap.timeline({
-    onUpdate: bToC,
-    repeat: -1,
-  });
-
-  tlB
-    // Snap to an initial position
-    // loop through positions until we get
-    // back to the inital position again
-    .to(boxB, {
-      ease: "none",
-      duration: 0,
-      x: "100%",
-      y: "100%",
-    })
-    .to(boxB, {
-      ease: "none",
-      duration: 8,
-      x: "50%",
-      y: "75%",
-    })
-    .to(boxB, {
-      ease: "none",
-      duration: 8,
-      x: "60%",
-      y: "55%",
-    })
-    .to(boxB, {
-      ease: "none",
-      duration: 6,
-      x: "80%",
-      y: "85%",
-    })
-    .to(boxB, {
-      ease: "none",
-      duration: 4,
-      x: "100%",
-      y: "100%",
-    });
-
-  const tlC = gsap.timeline({
-    onUpdate: CtoA,
-    repeat: -1,
-  });
-
-  tlC
-    // Snap to an initial position
-    // loop through positions until we get
-    // back to the inital position again
-    .to(boxC, {
-      ease: "none",
-      duration: 0,
-      x: "0%",
-      y: "100%",
-    })
-    .to(boxC, {
-      ease: "none",
-      duration: 16,
-      x: "80%",
-      y: "125%",
-    })
-    .to(boxC, {
-      ease: "none",
-      duration: 8,
-      x: "55%",
-      y: "55%",
-    })
-    .to(boxC, {
-      ease: "none",
-      duration: 6,
-      x: "30%",
-      y: "75%",
-    })
-    .to(boxC, {
-      ease: "none",
-      duration: 4,
-      x: "0%",
-      y: "100%",
-    });
+    // 2. Animate random movement
+    setTimeout(() => {
+      moveIt(image, container);
+    }, 25);
+  }
 });
