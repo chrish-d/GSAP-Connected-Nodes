83bd21f82791c14e8532d44fef7755666cdc1d7e
Chrish Dunne <hey@chrish.design>
2020-07-29 09:39:49 +0100

    Further changes

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
+const container = document.getElementById("alumni");
 let velX = 3;
 let velY = 2;
-const container = document.getElementById("alumni");
 
 // ====
 
-function setRandomPosition() {
-  const cntWidth = container.offsetWidth / 2;
-  const cntWidthNeg = (container.offsetWidth / 2) * -1;
-
-  const cntHeight = container.offsetHeight / 2;
-  const cntHeightNeg = (container.offsetHeight / 2) * -1;
-
-  const randWidth =
-    Math.floor(Math.random() * (cntWidth - cntWidthNeg)) + cntWidthNeg;
-  const randHeight =
-    Math.floor(Math.random() * (cntHeight - cntHeightNeg)) + cntHeightNeg;
-
-  const opts = {
-    cntWidth: cntWidth,
-    cntWidthNeg: cntWidthNeg,
-    cntHeight: cntHeight,
-    cntHeightNeg: cntHeightNeg,
-    randWidth: randWidth,
-    randHeight: randHeight,
+function randomLeftTop(el, img) {
+  const containerWidth = el.offsetWidth / 2;
+  const containerHeight = el.offsetHeight / 2;
+  const imageWidth = img.offsetWidth / 2;
+  const imageHeight = img.offsetHeight / 2;
+  return {
+    left: Math.floor(Math.random() * (containerWidth - imageWidth)),
+    top: Math.floor(Math.random() * (containerHeight - imageHeight)),
   };
-
-  return opts;
 }
 
 // ====
@@ -33,18 +20,22 @@ function setRandomPosition() {
 function hitBox(el, bnd) {
   if (el.offsetLeft <= 0 && velX < 0) {
     velX = -1 * velX;
+    console.log(`velX`, velX);
   }
 
   if (el.offsetLeft + el.offsetWidth >= bnd.offsetWidth) {
     velX = -1 * velX;
+    console.log(`velX`, velX);
   }
 
   if (el.offsetTop <= 0 && velY < 0) {
     velY = -1 * velY;
+    console.log(`velY`, velY);
   }
 
   if (el.offsetTop + el.offsetHeight >= bnd.offsetHeight) {
     velY = -1 * velY;
+    console.log(`velY`, velY);
   }
 }
 
@@ -69,19 +60,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 2. Do some stuff to our images
   for (image of images) {
-    // 1. Set a random position
-    image.setAttribute(
-      "style",
-      `left: ${setRandomPosition("alumni").randWidth}px`
-    );
-    image.setAttribute(
-      "style",
-      `top: ${setRandomPosition("alumni").randHeight}px`
-    );
+    const random = randomLeftTop(container, image);
 
-    // 2. Animate random movement
-    setTimeout(() => {
-      moveIt(image, container);
-    }, 25);
+    image.style.left = `${random.left}px`;
+    image.style.top = `${random.top}px`;
+
+    moveIt(image, container);
   }
 });
